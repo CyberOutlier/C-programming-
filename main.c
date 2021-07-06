e0f5ccff6bb0ddf2b5fa2243b35ceac5c6dcdc26
Winston Muijs <78045162+CyberOutlier@users.noreply.github.com>
2021-07-06 10:49:42 +0000

    Make a converter!

--- main.c
+++ main.c
@@ -2,7 +2,14 @@
 #include <stdlib.h>
 
 int main(void){
-  printf("Hello Winston\n");
-  printf("I'm a coder badass!\n");
+  printf("***^^***\n");
+  printf("Welcome to the Decimal to Hexadecimal converter!\n");
+  printf("Enter an integer:\n");
+  int decimal;
+  scanf("%d",&decimal);
+  printf("Decimal Representation:   %9d\n", decimal);
+  printf("Converted to Hexadecimal: %9x\n", decimal);
+  printf("***^^***\n");
   return 0;
 }
+
